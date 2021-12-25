57ba9ab365f7a01840930af22f20f11d4b8865c9
DouShaoxun <dousx_cn@163.com>
2021-12-25 14:58:49 +0800

    extends ConcurrentHashMap

--- src/main/java/cn/crduer/util/ExpiryMap.java
+++ src/main/java/cn/crduer/util/ExpiryMap.java
@@ -9,202 +9,164 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Author: cruder
  * @Date: 2021/12/24/16:51
  */
-public class ExpiryMap<K, V> implements Map<K, V> {
+public class ExpiryMap<K, V>  extends ConcurrentHashMap<K, V> {
+
+    private static final long serialVersionUID = 1L;
 
     /**
-     * 存放k，v
+     * default expiry time 2m
      */
-    private ConcurrentHashMap workMap;
-    /**
-     * value为到期时间
-     */
-    private ConcurrentHashMap expiryMap;
+    private long expiry = 1000 * 60 * 2;
 
-    /**
-     * 默认保存时间1分钟
-     */
-    private long expiry_time;
-
-    /**
-     * 默认保存时间 60 秒
-     */
-    private static final long DEFAULT_EXPIRY_TIME = 1000 * 60;
-
-
-    /**
-     * circulation 循环时间  默认5分钟
-     */
-    private static final long CIRCULATION_TIME = 1000 * 60 * 5;
-
-    /**
-     * delay 启动延迟时间  默认1分钟
-     */
-    private static final long DELAY = 1000 * 60;
-
+    private HashMap<K, Long> expiryMap = new HashMap<>();
 
     public ExpiryMap() {
-        /**
-         * 默认过期时间 60秒
-         */
-        this(16, 60);
+        super();
     }
 
     /**
-     * 单位秒
-     *
-     * @param initialCapacity 容量
-     * @param expiryTime      过期时间
+     * @param expiryTime 单位毫秒
+     */
+    public ExpiryMap(long expiryTime) {
+        this(1 << 4, expiryTime);
+    }
+
+    /**
+     * @param initialCapacity 初始容量
+     * @param expiryTime      单位毫秒
      */
     public ExpiryMap(int initialCapacity, long expiryTime) {
-        this.expiry_time = DEFAULT_EXPIRY_TIME;
-        if (expiryTime > 0) {
-            this.expiry_time = expiryTime * 1000;
-        }
-        workMap = new ConcurrentHashMap(initialCapacity);
-        expiryMap = new ConcurrentHashMap(initialCapacity);
-        // 利用定时任务 循环removeExpiryKeys()
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                removeExpiryKeys();
-            }
-        }, DELAY, CIRCULATION_TIME);
+        super(initialCapacity);
+        this.expiry = expiryTime;
     }
 
-    /**
-     * 使用 默认过期时间
-     *
-     * @param key   key
-     * @param value value
-     * @return V
-     */
     @Override
     public V put(K key, V value) {
-        expiryMap.put(key, System.currentTimeMillis() + expiry_time);
-        return (V) workMap.put(key, value);
-    }
-
-    /**
-     * 使用 自定义过期时间 单位秒
-     *
-     * @param key    key
-     * @param value  value
-     * @param exrity exrity
-     * @return
-     */
-    public V put(K key, V value, long exrity) {
-        if (exrity <= 0) {
-            exrity = this.expiry_time;
-        }
-        // 设置过期时间
-        expiryMap.put(key, System.currentTimeMillis() + exrity * 1000);
-        return (V) workMap.put(key, value);
-    }
-
-    /**
-     * 删除已到期的 键值对
-     */
-    private void removeExpiryKeys() {
-        expiryMap.keySet().forEach(key -> {
-            checkExpiry((K) key, true);
-        });
-    }
-
-    /**
-     * 是否过期判断函数
-     *
-     * @param key      key
-     * @param isDelete 是否删除
-     * @return 过期true  不过期false
-     */
-    private boolean checkExpiry(K key, boolean isDelete) {
-        Object timeObject = expiryMap.get(key);
-        if (timeObject == null) {
-            return true;
-        }
-        long setTime = (long) timeObject;
-        boolean isExpiry = System.currentTimeMillis() - setTime >= 0;
-        if (isExpiry) {
-            if (isDelete) {
-                expiryMap.remove(key);
-                workMap.remove(key);
-            }
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public V get(Object key) {
-        boolean isExpiry = checkExpiry((K) key, true);
-        if (isExpiry) {
-            return null;
-        }
-        return (V) workMap.get(key);
-    }
-
-    @Override
-    public int size() {
-        removeExpiryKeys();
-        return workMap.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        removeExpiryKeys();
-        return workMap.isEmpty();
+        expiryMap.put(key, System.currentTimeMillis() + expiry);
+        return super.put(key, value);
     }
 
     @Override
     public boolean containsKey(Object key) {
-        removeExpiryKeys();
-        return workMap.containsKey(key);
+        return !checkExpiry(key, true) && super.containsKey(key);
+    }
+
+    /**
+     * @param key        key
+     * @param value      value
+     * @param expiryTime 键值对有效期 毫秒
+     * @return value
+     */
+    public V put(K key, V value, long expiryTime) {
+        expiryMap.put(key, System.currentTimeMillis() + expiryTime);
+        return super.put(key, value);
+    }
+
+    @Override
+    public int size() {
+        return entrySet().size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return entrySet().size() == 0;
     }
 
     @Override
     public boolean containsValue(Object value) {
-        removeExpiryKeys();
-        return workMap.containsValue(value);
-    }
-
-    @Override
-    public V remove(Object key) {
-        expiryMap.remove(key);
-        return (V) workMap.remove(key);
-    }
-
-    @Override
-    public void putAll(Map<? extends K, ? extends V> m) {
-        m.entrySet().forEach(en -> {
-            expiryMap.put(en.getKey(), System.currentTimeMillis() + expiry_time);
-            workMap.put(en.getKey(), en.getValue());
-        });
-    }
-
-    @Override
-    public void clear() {
-        expiryMap.clear();
-        workMap.clear();
-    }
-
-    @Override
-    public Set<K> keySet() {
-        removeExpiryKeys();
-        return workMap.keySet();
+        if (value == null) {
+            return Boolean.FALSE;
+        }
+        Set<java.util.Map.Entry<K, V>> set = super.entrySet();
+        Iterator<java.util.Map.Entry<K, V>> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            java.util.Map.Entry<K, V> entry = iterator.next();
+            if (value.equals(entry.getValue())) {
+                if (checkExpiry(entry.getKey(), false)) {
+                    iterator.remove();
+                    return Boolean.FALSE;
+                }
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
     }
 
     @Override
     public Collection<V> values() {
-        removeExpiryKeys();
-        return workMap.values();
+
+        Collection<V> values = super.values();
+
+        if (values.size() < 1) {
+            return values;
+        }
+
+        values.removeIf(next -> !containsValue(next));
+        return values;
     }
 
     @Override
-    public Set<Entry<K, V>> entrySet() {
-        removeExpiryKeys();
-        return workMap.entrySet();
+    public V get(Object key) {
+        if (key == null) {
+            return null;
+        }
+        if (checkExpiry(key, true)) {
+            return null;
+        }
+        return super.get(key);
     }
 
+
+    public Object isInvalid(Object key) {
+        if (key == null) {
+            return null;
+        }
+        if (!expiryMap.containsKey(key)) {
+            return null;
+        }
+        long expiryTime = expiryMap.get(key);
+
+        boolean flag = System.currentTimeMillis() > expiryTime;
+
+        if (flag) {
+            super.remove(key);
+            expiryMap.remove(key);
+            return -1;
+        }
+        return super.get(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        for (Map.Entry<? extends K, ? extends V> e : m.entrySet()) {
+            expiryMap.put(e.getKey(), System.currentTimeMillis() + expiry);
+        }
+        super.putAll(m);
+    }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+        Set<java.util.Map.Entry<K, V>> set = super.entrySet();
+        set.removeIf(entry -> checkExpiry(entry.getKey(), false));
+        return set;
+    }
+
+
+    private boolean checkExpiry(Object key, boolean isRemoveSuper) {
+
+        if (!expiryMap.containsKey(key)) {
+            return Boolean.FALSE;
+        }
+        long expiryTime = expiryMap.get(key);
+
+        boolean flag = System.currentTimeMillis() > expiryTime;
+
+        if (flag) {
+            if (isRemoveSuper) {
+                super.remove(key);
+            }
+            expiryMap.remove(key);
+        }
+        return flag;
+    }
 }
-
-
